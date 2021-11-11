40b23514ea8f9a7f4a929ae87711922342a66376
Black-Swan-ICL <Black-Swan-ICL@users.noreply.github.com>
2021-11-11 13:44:17 +0000

    Updating the setup.py file prior to release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / 'README.md').read_text()
 
 setup(
     name='PyRKHSstats',
-    version='1.0.0',
+    version='1.1.0',
     description='A Python package for kernel methods in Statistics/ML.',
     long_description=README,
     long_description_content_type='text/markdown',
