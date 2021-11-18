eb2ce2c83f5b78cbd8bae2a6dff3964a5892b934
Black-Swan-ICL <Black-Swan-ICL@users.noreply.github.com>
2021-11-18 15:58:40 +0000

    Code fully cleaned now

--- PyRKHSstats/kcit_checks.py
+++ PyRKHSstats/kcit_checks.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from collections import namedtuple
+
 from scipy.spatial.distance import pdist
 from scipy.stats import norm, gamma, laplace
 from sklearn.gaussian_process.kernels import RBF
@@ -26,7 +28,8 @@ _field_histogram_xlabel = 'TCI'
 _field_histogram_ylabel = 'Probability density'
 
 
-def generate_low_dimensional_ci_example(nb_observations):
+# Low-dimensional settings example, under which CI holds true
+def generate_low_dim_ci_example(nb_observations):
 
     # Low-dimensional where CI holds true
     data_z = laplace.rvs(loc=0,
@@ -62,7 +65,8 @@ def generate_low_dimensional_ci_example(nb_observations):
     return example
 
 
-def generate_low_dimensional_not_ci_example(nb_observations):
+# Low-dimensional settings example, under which CI does NOT hold true
+def generate_low_dim_not_ci_example(nb_observations):
 
     # Low-dimensional where CI does not hold
     data_x = laplace.rvs(loc=0.5,
@@ -98,8 +102,8 @@ def generate_low_dimensional_not_ci_example(nb_observations):
     return example
 
 
-def generate_example_data(example_to_run, nb_observations, nb_simulations, scheme,
-                          test_level):
+def generate_example_data(example_to_run, nb_observations, nb_simulations,
+                          scheme, test_level):
 
     values_tci = np.zeros((nb_simulations, 1))
     values_threshold = np.zeros((nb_simulations, 1))
@@ -146,88 +150,122 @@ def generate_example_plots(data, xlabel, ylabel, title):
     return fig
 
 
+def run_example(example, id, regime, nb_observations, nb_simulations, scheme,
+                test_level, savedir):
+
+    # Generate data for the example and compute interesting metrics
+    start_time = time.time()
+    df_example = generate_example_data(
+        example_to_run=example,
+        nb_observations=nb_observations,
+        nb_simulations=nb_simulations,
+        scheme=scheme,
+        test_level=test_level
+    )
+    time_taken = time.time() - start_time
+    nb_h0_rejected = np.sum(df_example[_field_null_rejected].values)
+    # Persist the data
+    csv_filename = (
+            f'KCIT_{id}_{nb_observations}_observations' +
+            f'_{nb_simulations}_simulations_{test_level}_level.csv'
+    )
+    csv_filename = os.path.join(savedir, csv_filename)
+    df_example.to_csv(csv_filename, index=False)
+
+    # Draw the histogram
+    title = (
+        f'Empirical TCI density under {regime} with ' +
+        f'{nb_observations} observations, over {nb_simulations} simulations.'
+    )
+    fig = generate_example_plots(
+        data=df_example[_field_values_tci].values,
+        xlabel=_field_histogram_xlabel,
+        ylabel=_field_histogram_ylabel,
+        title=title
+    )
+    plot_filename = (
+        f'TCI_histogram_{id}_{nb_observations}_observations_' +
+        f'{nb_simulations}_simulations_{test_level}_test_level_run.png'
+    )
+    plot_filename = os.path.join(savedir, plot_filename)
+    fig.savefig(plot_filename)
+    plt.close()
+
+    return time_taken, nb_h0_rejected
+
+
 if __name__ == '__main__':
 
     savedir = os.path.join('checks', 'KCIT')
     os.makedirs(savedir, exist_ok=True)
 
-    nb_sim = 1000
+    schemes = [ImplementedKCITSchemes.GAMMA]
+    simulation_numbers = [100, 1000]
+    test_levels = [0.05, 0.01]
+    sample_sizes = [100, 200]
 
-    test_level = 0.01
-    N = 100
+    examples = dict()
+    example1 = namedtuple('id', 'regime')
+    example1.id = 'Example1'
+    example1.regime = 'H0'
+    example2 = namedtuple('id', 'regime')
+    example2.id = 'Example2'
+    example2.regime = 'H1'
+    examples[example1] = generate_low_dim_ci_example
+    examples[example2] = generate_low_dim_not_ci_example
 
-    start_time = time.time()
+    # Prepare for the collection of information about the checks
+    nb_checks = (
+            len(schemes) * len(simulation_numbers) * len(sample_sizes) *
+            len(test_levels) * len(examples.keys())
+    )
+    arr_id = np.ndarray(shape=(nb_checks,), dtype=object)
+    arr_regime =  np.ndarray(shape=(nb_checks,), dtype=object)
+    arr_nb_obs = np.zeros(nb_checks)
+    arr_level = np.zeros(nb_checks)
+    arr_nb_sims = np.zeros(nb_checks)
+    arr_scheme =  np.ndarray(shape=(nb_checks,), dtype=object)
+    arr_h0_reject = np.zeros(nb_checks)
+    arr_time = np.zeros(nb_checks)
 
-    # Example I : low-dimensional settings, CI holds true
-    df_example1 = generate_example_data(
-        example_to_run=generate_low_dimensional_ci_example,
-        nb_observations=N,
-        nb_simulations=nb_sim,
-        scheme=ImplementedKCITSchemes.GAMMA,
-        test_level=test_level
-    )
-    csv_filename = os.path.join(
-        savedir,
-        (
-                f'KCIT_example1_{N}_observations_{nb_sim}_simulations_' +
-                f'{test_level}_level.csv'
-        )
-    )
-    df_example1.to_csv(csv_filename, index=False)
+    iter = 0
 
-    title = f'Empirical TCI density under H0 with {N} observations, over ' \
-            f'{nb_sim} simulations.'
-    fig = generate_example_plots(
-        data=df_example1[_field_values_tci].values,
-        xlabel=_field_histogram_xlabel,
-        ylabel=_field_histogram_ylabel,
-        title=title
-    )
-    plot_filename = os.path.join(
-        savedir,
-        (
-            f'TCI_histogram_example1_{N}_observations_{nb_sim}_simulations_' +
-            f'{test_level}_test_level_run.png'
-        )
-    )
-    fig.savefig(plot_filename)
-    plt.close()
+    for scheme in schemes:
+        for nb_simulations in simulation_numbers:
+            for nb_observations in sample_sizes:
+                for test_level in test_levels:
+                    for example_setup, example in examples.items():
 
-    # Example II : low-dimensional, CI does not hold
-    df_example2 = generate_example_data(
-        example_to_run=generate_low_dimensional_not_ci_example,
-        nb_observations=N,
-        nb_simulations=nb_sim,
-        scheme=ImplementedKCITSchemes.GAMMA,
-        test_level=test_level
-    )
-    csv_filename = os.path.join(
-        savedir,
-        (
-                f'KCIT_example2_{N}_observations_{nb_sim}_simulations_' +
-                f'{test_level}_level.csv'
-        )
-    )
-    df_example2.to_csv(csv_filename, index=False)
+                        time_taken, nb_h0_rejected = run_example(
+                            example=example,
+                            id=example_setup.id,
+                            regime=example_setup.regime,
+                            nb_observations=nb_observations,
+                            nb_simulations=nb_simulations,
+                            scheme=scheme,
+                            test_level=test_level,
+                            savedir=savedir
+                        )
 
-    title = f'Empirical TCI density under H1 with {N} observations, over ' \
-            f'{nb_sim} simulations.'
-    fig = generate_example_plots(
-        data=df_example2[_field_values_tci].values,
-        xlabel=_field_histogram_xlabel,
-        ylabel=_field_histogram_ylabel,
-        title=title
-    )
-    plot_filename = os.path.join(
-        savedir,
-        (
-            f'TCI_histogram_example2_{N}_observations_{nb_sim}_simulations_' +
-            f'{test_level}_test_level_run.png'
-        )
-    )
-    fig.savefig(plot_filename)
-    plt.close()
+                        arr_id[iter] = example_setup.id
+                        arr_regime[iter] = example_setup.regime
+                        arr_nb_obs[iter] = nb_observations
+                        arr_level[iter] = test_level
+                        arr_nb_sims[iter] = nb_simulations
+                        arr_scheme[iter] = scheme
+                        arr_h0_reject[iter] = nb_h0_rejected
+                        arr_time[iter] = time_taken
 
-    end_time = time.time()
+                        iter += 1
 
-    print(end_time - start_time)
+    df_summary_checks = pd.DataFrame()
+    df_summary_checks['Example ID'] = arr_id
+    df_summary_checks['Regime'] = arr_regime
+    df_summary_checks['Sample Size'] = arr_nb_obs
+    df_summary_checks['Test Level'] = arr_level
+    df_summary_checks['Number Simulations'] = arr_nb_sims
+    df_summary_checks['Scheme'] = arr_scheme
+    df_summary_checks['Proportion H0 Rejected'] = arr_h0_reject / arr_nb_sims
+    df_summary_checks['Time Taken in seconds'] = arr_time
+    summary_filename = os.path.join(savedir, 'summary_checks.csv')
+    df_summary_checks.to_csv(summary_filename, index=False)
